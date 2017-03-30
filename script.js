2f7301632900f5aef723814ade2b4655b6af2572
rraider <hellmich.leon@gmail.com>
2017-03-30 19:54:31 +0200

    removed -add- function

---
+++ script.js
@@ -0,0 +1,72 @@
+var num1 = randomNumber();
+var num2 = randomNumber();
+var question = "Was ist " + num1 + " * " + num2 + " ?";
+
+
+
+
+
+function randomNumber() {
+    return Math.round(Math.random() * 9);
+}
+
+
+function randomNumberExtended() {
+    return Math.round(Math.random() * 20);
+}
+
+
+if (){
+
+}
+
+
+
+
+
+/* function onButtonClickAdd(e) {
+    e.preventDefault();
+    var userInput = answer.value;
+    if (userInput == num1 + num2) {
+
+        document.getElementById("output").innerHTML = (userInput + " ist richtig!");
+    } else {
+        document.getElementById("output").innerHTML = (userInput + " Ist Falsch!");
+    }
+    answer.value = "";
+    nextQuestionAdd();
+    return false;
+  }
+*/
+
+
+function onButtonClickMulti(e) {
+    e.preventDefault();
+    var userInput = answer.value;
+    if (userInput == num1 * num2) {
+
+        document.getElementById("output").innerHTML = (userInput + " ist richtig!");
+    } else {
+        document.getElementById("output").innerHTML = (userInput + " Ist Falsch!");
+    }
+    answer.value = "";
+    nextQuestionMulti();
+    return false;
+
+}
+
+function nextQuestionMulti() {
+    num1 = randomNumber();
+    num2 = randomNumber();
+    var question = "Was ist " + num1 + " * " + num2 + " ?";
+    document.getElementById("question").innerHTML = question;
+
+
+  /*  function nextQuestionAdd() {
+        num1 = randomNumber();
+        num2 = randomNumber();
+        var question = "Was ist " + num1 + " + " + num2 + " ?";
+        document.getElementById("question").innerHTML = question;
+
+}
+*/
