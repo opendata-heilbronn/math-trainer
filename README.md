b2f1f0b30cea22a1118e494b926179787ecc2acc
rraider <hellmich.leon@gmail.com>
2017-05-04 20:32:36 +0200

    variables into object

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 
 Online at https://opendata-heilbronn.github.io/math-trainer/
+
+# datastructure
+
+num1; num2; timeStart; timeEnd; userInput; expectedAnswer; timeDifferenceSeconds;
--- script.js
+++ script.js
@@ -1,48 +1,28 @@
-var num1 = randomNumber();
-var num2 = randomNumber();
-var datum1
-var datum2
-var question = "Was ist " + num1 + " * " + num2 + " ?";
-nextQuestion();
-
-
-
-
+var question;
+var answer = document.getElementById("answer");
 function randomNumber() {
     return Math.round(Math.random() * 9);
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 function onButtonClick(e) {
     e.preventDefault();
 
-    datum2 = new Date();
-    var userInput = answer.value;
-    var correctAnswer = num1 * num2;
-    if (userInput == num1 * num2) {
 
+    question.timeEnd = new Date();
+    question.userInput = answer.value;
+    question.expectedAnswer = question.num1 * question.num2;
+    question.correct = question.userInput === question.expectedAnswer;
+    question.timeDifference = question.timeEnd - question.timeStart;
+    if (question.correct) {
         document.getElementById("output").innerHTML = ("Richtig!");
     } else {
-
-        document.getElementById("output").innerHTML = (userInput + " Ist Falsch! Richtig wäre " + correctAnswer + "!");
+        document.getElementById("output").innerHTML = (question.userInput + " Ist Falsch! Richtig wäre " + question.expectedAnswer + "!");
 
     }
     answer.value = "";
-    var datumDifferenz = datum2 - datum1;
-    var datumDifferenzGerundet = datumDifferenz / 1000;
-    document.getElementById("time").innerHTML = ("Du hast " + datumDifferenzGerundet + " Sekunden gebraucht!");
+    var timeDifferenceSeconds = question.timeDifference / 1000;
+    document.getElementById("time").innerHTML = ("Du hast " + timeDifferenceSeconds + " Sekunden gebraucht!");
     nextQuestion();
     return false;
 
@@ -50,15 +30,16 @@ function onButtonClick(e) {
 }
 
 function nextQuestion() {
-    num1 = randomNumber();
-    num2 = randomNumber();
-    var question = "Was ist " + num1 + " * " + num2 + " ?";
-    document.getElementById("question").innerHTML = question;
-    datum1 = new Date();
+  question = {
+    num1 : randomNumber(),
+    num2 : randomNumber(),
+    timeStart : new Date()
+  }
+
+    var questionText = "Was ist " + question.num1 + " * " + question.num2 + " ?";
+    document.getElementById("question").innerHTML = questionText;
+
 }
-
-
-
 
 
 nextQuestion();
