bbba7ec96f433b6dc02d57436e6d6247bb0e330d
rraider <hellmich.leon@gmail.com>
2017-06-01 20:38:37 +0200

    added endcard

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var question;
+var challenge = {
+    maxMultiplier: 10,
+    maxQuestions: 10,
+};
+
 var totalTime = 0;
 var questionList = [];
 var answer = document.getElementById("answer");
+
 function randomNumber() {
-    return Math.round(Math.random() * 9);
+    return Math.round(Math.random() * (challenge.maxMultiplier - 1)) + 1;
 }
 
 
@@ -28,21 +34,57 @@ function onButtonClick(e) {
     answer.value = "";
     totalTime += question.timeDifference;
     var timeDifferenceSeconds = question.timeDifference / 1000;
-    document.getElementById("time").innerHTML = ("Du hast " + timeDifferenceSeconds + " Sekunden gebraucht! Durchschnitt: " + Math.round((totalTime/questionList.length)/100)/10 + " Sekunden!");
-    $("#results").append($("<tr><td>" + question.num1 + " * " + question.num2 + "</td><td>" + question.expectedAnswer + "</td><td>" + question.userInput + "</td><td>" + question.timeDifference + "</td><td>" + question.correct + "</td></tr>"));
+    var timeAverage = Math.round((totalTime / questionList.length) / 100) / 10;
+    document.getElementById("time").innerHTML = ("Du hast " + timeDifferenceSeconds +
+        " Sekunden gebraucht! Durchschnitt: " + timeAverage + " Sekunden!");
+    $("#results").append($("<tr><td>" + question.num1 + " * " + question.num2 +
+        "</td><td>" + question.expectedAnswer + "</td><td>" + question.userInput +
+        "</td><td>" + question.timeDifference + "</td><td>" + question.correct + "</td></tr>"));
 
-    nextQuestion();
+
+    if (questionList.length >= challenge.maxQuestions) {
+
+        $("#view").hide();
+        $("#outcome").show();
+        $("#yourTime").text(totalTime / 1000);
+        $("#yourAverage").text(timeAverage);
+
+        var correctAnswers = 0;
+
+        questionList.forEach(function (element){
+          if (element.correct) {
+            correctAnswers++;
+          }});
+
+        var correctAnswersInPercent = (correctAnswers / questionList.length) * 100;
+        $("#correctAnswersInPercent").text(correctAnswersInPercent);
+
+
+
+
+
+
+
+    } else {
+      nextQuestion();
+    }
+
+
+
+
+
+
     return false;
 
 
 }
 
 function nextQuestion() {
-  question = {
-    num1 : randomNumber(),
-    num2 : randomNumber(),
-    timeStart : new Date()
-  }
+    question = {
+        num1: randomNumber(),
+        num2: randomNumber(),
+        timeStart: new Date()
+    }
 
     var questionText = "Was ist " + question.num1 + " * " + question.num2 + " ?";
     document.getElementById("question").innerHTML = questionText;
